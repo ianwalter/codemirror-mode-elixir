fd030fd6356a1000b9704c838059e0d569faaeac
Ian Walter <public@iankwalter.com>
2016-11-17 12:06:04 -0500

    Fixing SVG whitespace issue

--- src/components/SiteFooter.vue
+++ src/components/SiteFooter.vue
@@ -1,8 +1,7 @@
 <template>
   <footer class="textCenter paddingTop50 paddingBottom100">
-    &copy; 2016 <a class="link border" href="https://iankwalter.com">
-      Ian Walter
-    </a>.
+    &copy; 2016
+    <a class="link border" href="https://iankwalter.com">Ian Walter</a>.
     All rights reserved.
   </footer>
 </template>
